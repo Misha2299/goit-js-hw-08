aa8a671c2f7cd0f510557d91aec298cc72a3ba04
Misha <130393993+Misha2299@users.noreply.github.com>
2023-11-27 22:52:59 +0200

    Add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from "lodash.throttle";
 
+const FEEDBACK_FORM_STATE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', formSubmit);
+form.addEventListener('input', throttle(formInput, 500));
+
+let messgForm = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE_KEY)) || {};
+const { email, message } = form.elements;
+populateMessage();
+
+function formInput() {
+    messgForm = { email: email.value, message: message.value };
+    localStorage.setItem(FEEDBACK_FORM_STATE_KEY, JSON.stringify(messgForm));
+}
+  
+function formSubmit(evt) {
+    evt.preventDefault();
+    console.log({ email: email.value, message: message.value });
+  
+    localStorage.removeItem(FEEDBACK_FORM_STATE_KEY);
+    evt.currentTarget.reset();
+}
+  
+function populateMessage() {
+    if (messgForm) {
+      email.value = messgForm.email || '';
+      message.value = messgForm.message || '';
+    }
+}
+ 
